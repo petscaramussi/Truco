b0965c938e491dc228d5e98edca10d4d778af89c
petscaramussi <44592787+petscaramussi@users.noreply.github.com>
2020-05-12 10:07:48 -0300

    changes

--- app/src/main/java/com/example/truco/MainActivity.java
+++ app/src/main/java/com/example/truco/MainActivity.java
@@ -37,18 +37,72 @@ public class MainActivity extends AppCompatActivity implements ExempleDialog.Exa
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Button btnAdd1 = findViewById(R.id.button);
-        final Button btnnAdd2 = findViewById(R.id.button2);
-        Button Truco = findViewById(R.id.TrucoBtn);
-        final TextView tv1 = findViewById(R.id.textView3);
-        final TextView tv2 = findViewById(R.id.textView2);
+        final Button Truco = findViewById(R.id.TrucoBtn);
         final Intent win = new Intent(this, WinActivity.class);
         final Button btnMenos1 = findViewById(R.id.button3);
         final Button btnMenos2 = findViewById(R.id.button4);
         name1 = (TextView) findViewById(R.id.tvTimeName);
         name2 = (TextView) findViewById(R.id.tvTimeName2);
+        final Button pl = findViewById(R.id.button5);
+        final Button pl2 = findViewById(R.id.button6);
 
         trucoM = 1;
+
+        pl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int teste2 = trucoM + placar2;
+                if(teste2 < 12) {
+                    if (placar2 < 11) {
+                        placar2 = placar2 + trucoM;
+                        pl2.setText("" + placar2);
+
+                    } else {
+                        pl2.setText(""+12);
+                        win.putExtra("win1", name2.getText());
+                        startActivity(win);
+
+
+                    }
+                }else{
+                    pl2.setText(""+12);
+                    win.putExtra("win1", name2.getText());
+                    startActivity(win);
+                }
+                Truco.setText("Truco");
+                trucoM = 1;
+            }
+        });
+
+        pl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int teste = trucoM + placar1;
+                if(teste < 12) {
+                    if (placar1 < 11) {
+                        placar1 = placar1 + trucoM;
+                        pl.setText(""+placar1);
+
+                    } else {
+                        pl.setText(""+12);
+                        win.putExtra("win1", name1.getText());
+                        startActivity(win);
+
+
+
+                    }
+                }else{
+                    pl.setText(""+12);
+                    win.putExtra("win1", name1.getText());
+                    startActivity(win);
+
+
+                }
+                Truco.setText("Truco");
+                trucoM =1;
+
+            }
+        });
 
         //abrir modal quando o texto de nome for clicado
         name1.setOnClickListener(new View.OnClickListener() {
@@ -69,82 +123,25 @@ public class MainActivity extends AppCompatActivity implements ExempleDialog.Exa
         });
 
 
-        btnAdd1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int teste = trucoM + placar1;
-                if(teste < 12) {
-                    if (placar1 < 11) {
-                        placar1 = placar1 + trucoM;
-                        tv1.setText("" + placar1);
-
-                    } else {
-                        tv1.setText(""+12);
-                        win.putExtra("win1", name1.getText());
-                        startActivity(win);
 
 
-
-                    }
-                }else{
-                    tv1.setText(""+12);
-                    win.putExtra("win1", name1.getText());
-                    startActivity(win);
-
-
-                }
-                btnnAdd2.setText("+1");
-                btnAdd1.setText("+1");
-                trucoM =1;
-            }
-        });
-
-        btnnAdd2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int teste2 = trucoM + placar2;
-                if(teste2 < 12) {
-                    if (placar2 < 11) {
-                        placar2 = placar2 + trucoM;
-                        tv2.setText("" + placar2);
-
-                    } else {
-                        tv2.setText(""+12);
-                        win.putExtra("win1", name2.getText());
-                        startActivity(win);
-
-
-                    }
-                }else{
-                    tv2.setText(""+12);
-                    win.putExtra("win1", name2.getText());
-                    startActivity(win);
-                }
-                btnAdd1.setText("+1");
-                btnnAdd2.setText("+1");
-                trucoM = 1;
-            }
-        });
 
         Truco.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              if(btnnAdd2.getText() == "+3" && btnAdd1.getText() == "+3"){
-                  btnnAdd2.setText("+6");
-                  btnAdd1.setText("+6");
-              }else if(btnnAdd2.getText() == "+6" && btnAdd1.getText() == "+6"){
-                  btnnAdd2.setText("+9");
-                  btnAdd1.setText("+9");
+
+                if(Truco.getText() == "Truco 9"){
+                    Truco.setText("Truco 12");
                 }
-              else if(btnnAdd2.getText() == "+9" && btnAdd1.getText() == "+9"){
-                  btnnAdd2.setText("+12");
-                  btnAdd1.setText("+12");
-              }
-              else{
-                  btnAdd1.setText("+3");
-                  btnnAdd2.setText("+3");
-              }
+                else if(Truco.getText() == "Truco 6"){
+                    Truco.setText("Truco 9");
+                }
+                else if(Truco.getText() == "Truco 3"){
+                    Truco.setText("Truco 6");
+                }else {
+                    Truco.setText("Truco 3");
+                }
+
                 if(trucoM == 1){
                 trucoM = trucoM + 2;
                 }else{
@@ -154,7 +151,7 @@ public class MainActivity extends AppCompatActivity implements ExempleDialog.Exa
                     if(trucoM < 11){
                     trucoM = trucoM + 3;
                     }
-                }
+                    }
 
 
             }
@@ -165,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements ExempleDialog.Exa
             public void onClick(View v) {
                 if(placar1 >= 1) {
                     placar1 = placar1 - 1;
-                    tv1.setText("" + placar1);
+                    pl.setText("" + placar1);
                 }
             }
         });
@@ -175,7 +172,7 @@ public class MainActivity extends AppCompatActivity implements ExempleDialog.Exa
             public void onClick(View v) {
                 if(placar2 >= 1) {
                     placar2 = placar2 - 1;
-                    tv2.setText("" + placar2);
+                    pl2.setText("" + placar2);
                 }
 
             }
